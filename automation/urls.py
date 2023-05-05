3c857466438a057d6c00093ddcc2cbd4403d8cc5
Jeff Triplett <jeff.triplett@gmail.com>
2023-05-04 22:30:39 -0500

    :shirt: Lints

--- automation/urls.py
+++ automation/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 from django.views.generic import TemplateView
 
 from titowebhooks.views import tito_webhook
-
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="homepage.html")),
