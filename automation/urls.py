7c713daaea3423980edefc8e0d8d69f087267e45
Jeff Triplett <jeff.triplett@gmail.com>
2023-05-06 13:19:11 -0500

    :pencil: Adds version support in admin

--- automation/urls.py
+++ automation/urls.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
 
+from automation import __version__
 from titowebhooks.views import tito_webhook
+
+admin_header = f"DjangoCon US Automation v{__version__}"
+admin.site.enable_nav_sidebar = False
+admin.site.site_header = admin_header
+admin.site.site_title = admin_header
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="homepage.html")),
