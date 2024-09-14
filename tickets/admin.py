dcac26007ed7cc76bbb070ab1276706bceb7cede
Jeff Triplett <jeff.triplett@gmail.com>
2024-09-13 20:53:25 -0500

    :gear: Adds TicketLinkAdmin

--- tickets/admin.py
+++ tickets/admin.py
@@ -1,1 +1,24 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import TicketLink
+
+
+@admin.register(TicketLink)
+class TicketLinkAdmin(admin.ModelAdmin):
+    list_display = ("link", "date_link_created", "date_link_accessed", "is_accessed")
+    list_filter = ("date_link_created", "date_link_accessed")
+    search_fields = ("link",)
+    readonly_fields = ("date_link_created", "date_link_accessed")
+    ordering = ("-date_link_created",)
+
+    @admin.display(
+        description="Accessed",
+        boolean=True,
+    )
+    def is_accessed(self, obj):
+        return obj.date_link_accessed is not None
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:  # When editing an existing object
+            return self.readonly_fields + ("link",)
+        return self.readonly_fields
